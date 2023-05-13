c5a0aaf9eeac554d587aa7175f946809ed739129
Vincent Ocquet <vincent.ocquet@gmail.com>
2023-05-13 12:58:13 +0200

    fix #25 fallback to message output if there's no code change

--- src/main/java/com/vcque/prompto/actions/PromptoImplementAction.java
+++ src/main/java/com/vcque/prompto/actions/PromptoImplementAction.java
@@ -2,15 +2,20 @@ package com.vcque.prompto.actions;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiMethod;
 import com.vcque.prompto.PromptoResponse;
 import com.vcque.prompto.Utils;
-import com.vcque.prompto.contexts.*;
+import com.vcque.prompto.contexts.AvailableClassesRetriever;
+import com.vcque.prompto.contexts.EditorContentRetriever;
+import com.vcque.prompto.contexts.LanguageRetriever;
+import com.vcque.prompto.contexts.MethodRetriever;
+import com.vcque.prompto.contexts.SettingsRetriever;
 import com.vcque.prompto.outputs.MethodOutput;
-import com.vcque.prompto.pipelines.PromptoRetrieverDefinition;
 import com.vcque.prompto.pipelines.PromptoPipeline;
+import com.vcque.prompto.pipelines.PromptoRetrieverDefinition;
 
 import java.util.List;
 
@@ -29,26 +34,47 @@ public class PromptoImplementAction extends PromptoAction<PromptoResponse> {
                 ))
                 .defaultInput("Implement this method to the best of your abilities")
                 .output(new MethodOutput())
-                .execution((result, scope) -> {
-                    var project = scope.project();
-                    var editorBlock = result.firstBlock();
-                    if (editorBlock.isEmpty()) {
-                        ApplicationManager.getApplication().invokeLater(() -> Messages.showInfoMessage(result.getRaw(), "Prompto"));
-                    } else {
-                        WriteCommandAction.runWriteCommandAction(project, () -> {
-                            var code = editorBlock.get().code();
-                            var newPsiClass = Utils.asPsiClass(code, project);
-                            var oldPsiClass = Utils.findParentOfType(scope.element(), PsiClass.class);
-                            var oldMethod = Utils.findParentOfType(scope.element(), PsiMethod.class);
-                            if (newPsiClass.getMethods().length > 0) {
-                                var newMethod = newPsiClass.getMethods()[0];
-                                oldMethod.replace(newMethod);
-                            }
-                            Utils.mergePsiClasses(oldPsiClass, newPsiClass);
-                        });
-                    }
-                })
+                .execution(this::apply)
                 .build();
+    }
+
+    private void apply(PromptoResponse result, PromptoPipeline.Scope scope) {
+        var project = scope.project();
+        if (result.getEditorBlocks().isEmpty()) {
+            ApplicationManager.getApplication().invokeLater(() -> Messages.showInfoMessage(result.getRaw(), "Prompto"));
+        } else {
+            WriteCommandAction.runWriteCommandAction(project, () -> {
+                var hasChanged = insertCode(scope, result);
+                if (!hasChanged) {
+                    ApplicationManager.getApplication().invokeLater(() -> Messages.showInfoMessage(result.getRaw(), "Prompto"));
+                }
+            });
+        }
+    }
+
+    private boolean insertCode(PromptoPipeline.Scope scope, PromptoResponse response) {
+        var methodHolder = findEditorBlockWithMethods(scope.project(), response);
+        if (methodHolder == null) {
+            return false;
+        }
+        var oldPsiClass = Utils.findParentOfType(scope.element(), PsiClass.class);
+        var oldMethod = Utils.findParentOfType(scope.element(), PsiMethod.class);
+        var newMethod = methodHolder.getMethods()[0];
+        oldMethod.replace(newMethod);
+        Utils.mergePsiClasses(oldPsiClass, methodHolder);
+        return true;
+    }
+
+    private PsiClass findEditorBlockWithMethods(Project project, PromptoResponse response) {
+        var editorBlocks = response.getEditorBlocks();
+        for (var editorBlock : editorBlocks) {
+            var code = editorBlock.code();
+            var psiClass = Utils.asPsiClass(code, project);
+            if (psiClass.getMethods().length > 0) {
+                return psiClass;
+            }
+        }
+        return null;
     }
 
 }
