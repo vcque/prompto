189c60cd6c2dc1b9c1bb59f8e9a077ba95d1d0bc
Vincent Ocquet <vincent.ocquet@gmail.com>
2023-05-16 21:39:47 +0200

    remove the clipboard action, the prompto query dialog does that instead

--- src/main/java/com/vcque/prompto/PromptoManager.java
+++ src/main/java/com/vcque/prompto/PromptoManager.java
@@ -16,7 +16,6 @@ import com.vcque.prompto.exceptions.MissingTokenException;
 import com.vcque.prompto.pipelines.PromptoPipeline;
 import com.vcque.prompto.settings.PromptoSettingsState;
 import com.vcque.prompto.ui.PromptoQueryDialog;
-import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.datatransfer.StringSelection;
@@ -24,7 +23,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class PromptoManager {
@@ -135,40 +133,5 @@ public class PromptoManager {
         var extractedResult = pipeline.getOutput().extractOutput(response);
         // Execute the action
         pipeline.getExecution().execute(extractedResult, scope, contexts);
-    }
-
-    public <T> String buildManualPrompt(PromptoPipeline<T> pipeline, List<PromptoContext> contexts, String userInput) {
-        var maxToken = 2000; // To configure, this is ~ the number of token allowed in the chatGPT webapp
-
-        var chatMessages = new ArrayList<ChatMessage>();
-        chatMessages.add(Prompts.codingAssistant());
-
-
-        chatMessages.addAll(
-                contexts.stream()
-                        .takeWhile(new MaxTokenPredicate(maxToken))
-                        .map(Prompts::promptoContext)
-                        .toList()
-        );
-        chatMessages.add(pipeline.getOutput().chatMessage());
-        chatMessages.add(Prompts.userInput(userInput));
-
-        return chatMessages.stream()
-                .map(ChatMessage::getContent)
-                .collect(Collectors.joining("\n"));
-    }
-
-    @RequiredArgsConstructor
-    private static class MaxTokenPredicate implements Predicate<PromptoContext> {
-
-        private final int maxToken;
-        private int currentToken = 0;
-
-        @Override
-        public boolean test(PromptoContext context) {
-            // Stateful predicate FTW
-            currentToken += context.cost();
-            return currentToken < maxToken;
-        }
     }
 }
